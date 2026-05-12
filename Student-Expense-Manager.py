10184661817c34edc98a8e6d7863efa00e24cfe3
Charan Teja Kushuma <tejac7362@gmail.com>
2026-05-12 11:44:15 +0530

    Task-1

---
+++ Student-Expense-Manager.py
@@ -0,0 +1,7 @@
+def ExpenseManager():
+    print("Welcome to the Student Expense Manager!")
+ExpenseManager()
+
+Amount = input("Enter the amount you spend on this month: ")
+StudentName = input("Enter your name: ")
+print("Hello " + StudentName + "! You have spent $" + Amount + " this month.")
